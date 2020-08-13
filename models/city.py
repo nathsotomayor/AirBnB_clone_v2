f737df6a381a4fbe80b5c836a6aff4f0f46b11af
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-13 21:32:18 +0000

    Change order of fields in table Cities

--- models/city.py
+++ models/city.py
@@ -3,14 +3,11 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    # state = relationship('State', back_populates='cities',
-    # cascade='all, delete')
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
