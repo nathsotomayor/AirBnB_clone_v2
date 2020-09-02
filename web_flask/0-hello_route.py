ecfbe9f4134165ee934ce8f0afb5e6586bbc58a5
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-02 16:31:30 -0500

    Hello Flask! Script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that starts a Flask application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Function that generates the route"""
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run()
