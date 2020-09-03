88a73316bccbe50b7a011c62e251acd8302b6e99
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-02 23:26:57 -0500

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that starts a Flask application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown():
+    """Remove current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """Enables '/states_list/' route"""
+    return render_template('7-states_list.html',
+                           states=storage.all(State).values())
+
+
+if __name__ == "__main__":
+    app.run()
