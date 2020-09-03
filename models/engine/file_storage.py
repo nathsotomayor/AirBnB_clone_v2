c834dec9120ba0f824f356dd94c285ad3c47259b
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-02 22:49:28 -0500

    Improve engines to display data with flask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,7 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
             self.save()
+
+    def close(self):
+        """Deserializing the JSON file to objects calling 'reload()' method"""
+        reload(self)
